8b1b46609aab2d7e7202195cf768eb95b73514e1
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-06-19 06:29:46 +0600

    create view more menu button

--- src/pages/Home/popularMenu/PopularMenu.jsx
+++ src/pages/Home/popularMenu/PopularMenu.jsx
@@ -24,6 +24,11 @@ const PopularMenu = () => {
                     menu.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
                 }
             </div>
+            <div className='flex justify-center items-center'>
+                <button className="border-b-2 border-black hover:bg-orange-400 hover:text-white transition-all px-6 py-2 rounded-md mt-2">
+                    View More Menu
+                </button>
+            </div>
         </section>
     )
 }
