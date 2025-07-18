0129ac1372388c2cd05b3d63c4aa177bbe8dc3d2
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-07-18 20:06:46 +0600

    menu and reviews data loaded mongodb

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -10,7 +10,7 @@ import '@smastrom/react-rating/style.css'
 const Testimonials = () => {
     const [reviews, setReviews] = useState([])
     useEffect(() => {
-        fetch('reviws.json')
+        fetch('http://localhost:3000/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
