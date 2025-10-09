09cff9c0466587ebf92bf6d56aad2628af361ee7
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-10-09 08:13:12 +0600

    Add recharts for data visualization in AdminHome; implement bar chart for order state

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,6 +1,10 @@
 import { FaDollarSign, FaUsers, FaBoxOpen, FaShoppingCart } from "react-icons/fa";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
 
 const AdminHome = () => {
 
@@ -14,6 +18,25 @@ const AdminHome = () => {
         }
     })
 
+    const { data: chartData = [] } = useQuery({
+        queryKey: ['order state'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/order-state')
+            return res.data;
+        }
+    })
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
 
     return (
         <div className="p-6">
@@ -62,6 +85,36 @@ const AdminHome = () => {
                         <h3 className="text-lg font-semibold">Orders</h3>
                         <p className="text-2xl font-bold mt-1">{states.orders}</p>
                     </div>
+                </div>
+            </div>
+
+            {/* chart data */}
+
+            <div className="flex">
+                <div className="w-1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                <div>
+
                 </div>
             </div>
         </div>
